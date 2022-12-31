8802f8b51bf0fc06d0cba464c213969e5e483942
piyushshri01 <piyushshrimal94@gmail.com>
2022-12-31 08:34:50 +0530

    test

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+import * as dotenv from 'dotenv'
 const path = require('path')
 const multer  = require('multer')
 const upload = multer({ dest: 'uploads/' })
@@ -6,21 +7,19 @@ const {mergePdfs} = require("./merge")
 const app = express()
 // app.use('/static', express.static('public'))
 
-const port = 3000
-
 app.get('/', (req, res) => {
   // res.sendFile(path.join(__dirname, "templates/index.html"))
   res.json({message:"hello"})
 })
 
-// app.post('/merge', upload.array('pdfs', 2), async (req, res, next)=> {
-//     // console.log(req.files);
-//     let d = await mergePdfs(path.join(__dirname, req.files[0].path), path.join(__dirname, req.files[1].path))
-//     // await res.redirect(`http://localhost:3000/static/${d}.pdf`)
-//     // res.send({data:req.files})
-//     // req.files is array of `pdfs` files
-//     // req.body will contain the text fields, if there were any
-// })
+app.post('/merge', upload.array('pdfs', 2), async (req, res, next)=> {
+    // console.log(req.files);
+    let d = await mergePdfs(path.join(__dirname, req.files[0].path), path.join(__dirname, req.files[1].path))
+    await res.redirect(`http://localhost:3000/static/${d}.pdf`)
+    // res.send({data:req.files})
+    // req.files is array of `pdfs` files
+    // req.body will contain the text fields, if there were any
+})
 
 app.listen(3000, () => {
   console.log(`Example app listening on port ${port}`)
