0b2c64904e5d3aef7b19210c65853decc6b5bfb4
piyushshri01 <piyushshrimal94@gmail.com>
2022-12-31 08:11:48 +0530

    testing

--- merge.js
+++ merge.js
@@ -7,7 +7,7 @@ const mergePdfs = async (p1, p2) => {
   await merger.add(p1);  //merge all pages. parameter is the path to file and filename.
   await merger.add(p2);  //merge all pages. parameter is the path to file and filename.
   let d = new Date().getTime()
-  await merger.save(`public/${d}.pdf`); //save under given name and reset the internal document
+  // await merger.save(`public/${d}.pdf`); //save under given name and reset the internal document
   return d
   // Export the merged PDF as a nodejs Buffer
   // const mergedPdfBuffer = await merger.saveAsBuffer();
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const multer  = require('multer')
 const upload = multer({ dest: 'uploads/' })
 const {mergePdfs} = require("./merge")
 const app = express()
-app.use('/static', express.static('public'))
+// app.use('/static', express.static('public'))
 
 const port = 3000
 
@@ -12,15 +12,15 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "templates/index.html"))
 })
 
-app.post('/merge', upload.array('pdfs', 2), async (req, res, next)=> {
-    // console.log(req.files);
-    let d = await mergePdfs(path.join(__dirname, req.files[0].path), path.join(__dirname, req.files[1].path))
-    await res.redirect(`http://localhost:3000/static/${d}.pdf`)
-    // res.send({data:req.files})
-    // req.files is array of `pdfs` files
-    // req.body will contain the text fields, if there were any
-})
+// app.post('/merge', upload.array('pdfs', 2), async (req, res, next)=> {
+//     // console.log(req.files);
+//     let d = await mergePdfs(path.join(__dirname, req.files[0].path), path.join(__dirname, req.files[1].path))
+//     // await res.redirect(`http://localhost:3000/static/${d}.pdf`)
+//     // res.send({data:req.files})
+//     // req.files is array of `pdfs` files
+//     // req.body will contain the text fields, if there were any
+// })
 
-app.listen(port, () => {
+app.listen(3000, () => {
   console.log(`Example app listening on port ${port}`)
 })
