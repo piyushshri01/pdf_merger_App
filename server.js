fba9c91982edd4e129d37ceedc204c436d88cf09
piyushshri01 <piyushshrimal94@gmail.com>
2022-12-31 08:23:47 +0530

    test

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, "templates/index.html"))
+  // res.sendFile(path.join(__dirname, "templates/index.html"))
+  res.json({message:"hello"})
 })
 
 // app.post('/merge', upload.array('pdfs', 2), async (req, res, next)=> {
